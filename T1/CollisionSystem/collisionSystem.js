746f17453c00895c49c5c4bd35962f7f046ce099
Yan-Fonseca <66083422+Yan-Fonseca@users.noreply.github.com>
2024-04-07 21:03:59 -0300

    criado o método responsável por definir quais projéteis estão perto de quais tanques. O intuito é realizar uma verificação de colisão mais detalhada utilizando esse objeto

--- T1/CollisionSystem/collisionSystem.js
+++ T1/CollisionSystem/collisionSystem.js
@@ -13,8 +13,30 @@ class CollisionSystem {
 }
 
 class ProjectileCollisionSystem extends CollisionSystem {
-    constructor(players, map) {
+    constructor(players, map, minDistanceForDanger=5) {
         super(players, map);
+        this.minDistanceForDanger = minDistanceForDanger;
+        this.projectiles = [];
+        this.projectilesNearPlayers = {}; // chave: índice do jogador, valor: lista de projéteis próximos.
+    }
+
+    addProjectile(projectile) {
+        this.projectiles.push(projectile);
+    }
+
+    checkForProjectilesNearPlayers() {
+        let players = this.players;
+        let projectiles = this.projectiles;
+        let minDistanceForDanger = this.minDistanceForDanger;
+        let projectilesNearPlayer;
+        
+        for (let playerIndex = 0; playerIndex < players.length; playerIndex++) {
+            projectilesNearPlayer = projectiles.filter((projectile) => {
+                return players[playerIndex].position.distanceTo(projectile.position) < minDistanceForDanger;
+            });
+            this.projectilesNearPlayers[playerIndex] = projectilesNearPlayer;
+            projectilesNearPlayer = [];
+        }
     }
 }
 
